d869300a2b16fef66092fe9c88d5f7303cb212d1
Stascha <stasarasic@gmail.com>
2018-04-15 23:55:12 +0200

    Update function.java

--- function.java
+++ function.java
@@ -6,20 +6,11 @@ public class Main {
 
     public String function(HashSet<String> Dictionary, String inputString) {
 
-        // niz koji popunjavamo na sledeci nacin
-        // step[i] = ukoliko ne postoji nacin da se prefiks od prvih i slova
-        // stringa inputString isece na reci, onda je vrednost -1
-
-        // step[0] = definisemo na 0 (prazan string moze da se podeli)
-
-        // inace je vrednost j >= 0, i to znaci da sva slova unutar [j, i) cine
-        // jednu validnu rec i da step[j] moze da se isece moze i j = 0
-
-        // step[i] = j to znači da u inputStringu reč počinje od indexa j i ide do indexa i
+        // step[i] = j This means that in the inputString the word begins with index j and goes til the index i
         // inputString.substring(j, i)
-        // indexi do kojih se ne zvršava ni jedna reč niz step u tim indexima ima vrednost -1
-        // na primer za inputString = todayisfriday step[] = [0,-1,0,-1,-1,0,-1,5,-1,-1,-1,-1,-1,7]
-        // za inputString = tomorrowissaturday imamo step[] = [0,-1,0,-1,-1,-1,-1,-1,0,-1,8,-1,-1,-1,-1,-1,-1,-1,10]
+        // step[i] = -1 means there is no word which exactly ending before the index i
+        // example for inputString = todayisfriday step[] = [0,-1,0,-1,-1,0,-1,5,-1,-1,-1,-1,-1,7]
+        // example for inputString = tomorrowissaturday imamo step[] = [0,-1,0,-1,-1,-1,-1,-1,0,-1,8,-1,-1,-1,-1,-1,-1,-1,10]
 
         int[] step = new int[inputString.length() + 1];
 
@@ -39,17 +30,17 @@ public class Main {
             }
         }
 
-        // ako ne moze da se napravi od celog stringa, vrati prazan string
+        // if text can not be made from the whole string, return an empty string
         if (step[inputString.length()] == -1)
             return "";
 
-        // Kreci se unazad od kraja, rekonstruisi, words sadrzi reci u obrnutom redosledu
+        // Moving backwards from the end, the words contain words in the reverse order
         ArrayList<String> words = new ArrayList<String>();
 
         for (int x = inputString.length(); x > 0; x = step[x])
             words.add(inputString.substring(step[x], x));
 
-        // Napravi string pomocu string builder-a koji sadrzi reci u normalnom redosledu
+        // Create a string using a StringBuilder that contains words in the normal order
         StringBuilder sb = new StringBuilder();
 
         for (int i = words.size()-1; i >= 0; i--)
